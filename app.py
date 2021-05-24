d8d7356b85a7f1cb9e9ec2d437fa89df3a6f1b08
Daniel Merlin Benedetti <benedetti@gmail.com>
2021-05-24 09:22:16 -0300

    Demonstração

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "É Nois na Fita!!!"
+    return "É Nois na Fita!!! E vamos lá"
 
 if __name__ == '__main__':
     app.run(debug=True)
