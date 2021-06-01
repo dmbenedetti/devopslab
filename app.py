10f42540757a7eb0ffe2a0b327543a2904b77acc
Daniel Merlin Benedetti <benedetti@gmail.com>
2021-05-31 21:14:26 -0300

    Bug Adicionado

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "E Nois na Fita!!! E vamos la"
 
+@app.route('/bug')
+def bad():
+    try:
+        raise TypeError()
+    except TypeError as e:
+        print(e)
+    except TypeError as e:
+        print("Duplicado, ou seja, nunca vai entrar aqui.")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
